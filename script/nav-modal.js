ba65f14894454536655b895f2bc429437e6346f9
Joseph Scoggins <borninthecaul@gmail.com>
2021-03-12 11:35:52 -0800

    adds icons

--- script/nav-modal.js
+++ script/nav-modal.js
@@ -1,18 +1,23 @@
 'use strict'
 
+var app_shell = document.querySelector('#app')
 var nav_open = document.querySelector('#nav-hex')
 var nav_close = document.querySelector('#close-modal')
 var nav_shell = document.querySelector('#nav-modal-shell')
 
 nav_open.addEventListener('click', function (event) {
-  nav_modal_toggle(this,nav_shell)
+  var dim = 0.3
+  nav_modal_toggle(this,nav_shell,dim)
 })
 
 nav_close.addEventListener('click', function (event) {
-  nav_modal_toggle(nav_shell,nav_open)
+  var bright = 1
+  nav_modal_toggle(nav_shell,nav_open,bright)
 })
 
-function nav_modal_toggle(hide_me,show_me) {
+function nav_modal_toggle(hide_me,show_me,opacity) {
   hide_me.style.display = 'none'
   show_me.style.display = 'block'
+  app_shell.style.opacity = opacity
+
 }
