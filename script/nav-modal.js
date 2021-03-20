9e9892f36536f1c1f8ff969b8ce301afd902f348
Joseph Scoggins <borninthecaul@gmail.com>
2021-03-20 16:18:51 -0700

    working subnav UX script

--- script/nav-modal.js
+++ script/nav-modal.js
@@ -4,22 +4,19 @@ var app_shell = document.querySelector('#app')
 var nav_open = document.querySelector('#nav-hex')
 var nav_close = document.querySelector('#close-nav-modal')
 var nav_shell = document.querySelector('#nav-modal-shell')
-
 var nav_options = document.querySelectorAll('.nav-anchor')
 
 nav_options.forEach( function (nav_opt) {
   nav_opt.addEventListener('click', function (event) {
     var self = this.innerText
     var toggle_arr = this.getAttribute('data-display').split(',')
-    var bright_arr = this.getAttribute('data-opacity').split(',')
     var decor_arr = this.getAttribute('data-decor').split(',')
     var subnav = document.querySelector('#' + this.id + '-list').parentElement
     nav_options.forEach( function (sibling) {
       var sibling_subnav = (document.querySelector('#' + sibling.id + '-list')) ?
         document.querySelector('#' + sibling.id + '-list').parentElement : null
       if (sibling.innerText!=self) {
-        sibling.style.opacity = '0.33'
-        sibling.setAttribute('data-opacity','10,3.3')
+        sibling.style.opacity = (toggle_arr[0]!='none') ? '0.33' : '1'
         if (sibling_subnav) {
           sibling_subnav.style.display = 'none'
           sibling.style.textDecoration = 'none'
@@ -32,10 +29,8 @@ nav_options.forEach( function (nav_opt) {
     this.style.textDecoration = decor_arr[0]
     this.style.opacity = '1'
     toggle_arr.reverse()
-    bright_arr.reverse()
     decor_arr.reverse()
     this.setAttribute('data-display',toggle_arr.join(','))
-    this.setAttribute('data-opacity',bright_arr.join(','))
     this.setAttribute('data-decor',decor_arr.join(','))
   })
 })
