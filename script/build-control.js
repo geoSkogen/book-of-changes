8146511772606b97e7e13910e2c9a4c975ec00d8
Joseph Scoggins <borninthecaul@gmail.com>
2021-03-28 18:31:05 -0700

    adds hex modal drilldown to builder

--- script/build-control.js
+++ script/build-control.js
@@ -1,5 +1,26 @@
 'use strict'
 
+var hex_names = document.querySelectorAll('.hex-name')
+var hex_modal = document.querySelector('#hex-modal')
+var hex_modal_closer = document.querySelector('#close-hex-modal')
+var hex_refresh_icon = document.querySelector('#hex-refresh')
+var modal_dom = {
+  h0 : document.querySelector('#hex-number'),
+  h1 : document.querySelector('#hex-name-title'),
+  h2 : document.querySelector('#hex-name-alt'),
+  h3 : document.querySelector('#hex-name-mod'),
+  articles : {
+    inner : {
+      desc: document.querySelector('#hex-meta-inner'),
+      lines : document.querySelector('#moving-lines-inner')
+    },
+    outer : {
+      desc: document.querySelector('#hex-meta-outer'),
+      lines : document.querySelector('#moving-lines-outer')
+    }
+  }
+}
+
 var all_lines = document.querySelectorAll('.line-frame')
 
 var line_builder = {
@@ -122,5 +143,107 @@ function print_moving_icon(arg) {
   var icon = document.querySelector('#hex-mover')
   icon.style.display=  (arg) ? 'block' : 'none'
 }
+
+function clear_modal_content() {
+  for (var i = 0 ; i < 4; i++) {
+    modal_dom['h'+i].innerHTML = ''
+  }
+  Object.keys(modal_dom.articles).forEach( function (side_key) {
+    Object.keys(modal_dom.articles[side_key]).forEach( function (content_key) {
+      modal_dom.articles[side_key][content_key].innerHTML = ''
+    })
+  })
+}
+
+function open_hex_modal() {
+  hex_modal.style.display = 'block'
+  app_shell.style.opacity = 0.33
+}
+
+function close_hex_modal() {
+  hex_modal.style.display = 'none'
+  app_shell.style.opacity = 1
+  clear_modal_content();
+}
+
+function get_hex_collection(event) {
+  var collection = {
+    number: -1,
+    names: '',
+    articles: {
+      inner: { desc:'', lines:[] },
+      outer: { desc:'', lines:[] }
+    }
+  }
+  var current_hex = []
+  var hex_index = -1
+  var objs = [hex_graph.lines_arr[0],hex_graph.lines_arr[1]]
+  var props = ['first','second']
+  var descs = {}
+  props.forEach( function (prop) {
+    var el_prop = event.target.id.replace('-hex-name','')
+    hex_index = (props.indexOf(el_prop)!=-1) ? props.indexOf(el_prop) : hex_index
+    current_hex = (event.target.id===prop+'-hex-name' && hex_index>-1) ?
+      objs[hex_index] : current_hex
+  })
+  console.log(current_hex)
+  console.log(event.target.id)
+  collection.number
+   = library.get_hex_index(current_hex)
+  collection.names = library.select_names(collection.number)
+  descs = library.select_text(collection.number)
+  collection.articles.inner.desc = descs.inner
+  collection.articles.outer.desc = descs.outer
+  collection.articles.inner.lines =
+    library.select_moving_lines(collection.number,hex_graph.moving_lines,1)
+  collection.articles.outer.lines =
+    library.select_moving_lines(collection.number,hex_graph.moving_lines,0)
+  return collection
+}
+
+function render_modal_text(collection) {
+  var names = (collection.names.length) ? collection.names.split(' | ') : []
+  for (var i = 0; i < names.length+1; i++) {
+    modal_dom['h'+i].innerHTML = (i) ? names[i-1] : collection.number
+  }
+  Object.keys(modal_dom.articles).forEach( function (side) {
+    var side_key = side
+    Object.keys(modal_dom.articles[side_key]).forEach( function (text) {
+      switch (text) {
+        case 'desc' :
+          modal_dom.articles[side_key][text].innerHTML = collection.articles[side_key][text]
+          break
+        case 'lines' :
+          var list = document.createElement('ul')
+          if (collection.articles[side_key][text].length) {
+            collection.articles[side_key][text].forEach( function (line_text) {
+              var line_text = document.createTextNode(line_text)
+              var line_el = document.createElement('li')
+              line_el.appendChild(line_text)
+              line_el.className = 'moving-line-item'
+              list.appendChild(line_el)
+              list.className = 'moving-lines-list'
+            })
+            modal_dom.articles[side_key][text].appendChild(list)
+          }
+          break
+        default :
+          console.log('irregular data collection object')
+      }
+    })
+  })
+}
+
+hex_names.forEach( function (hex_name_el) {
+    //
+  hex_name_el.addEventListener('click', function (event) {
+      //
+      var collection = get_hex_collection(event)
+      render_modal_text(collection)
+      open_hex_modal()
+  })//ends event listener function
+})//ends hex iteration
+
+hex_modal_closer.addEventListener('click',close_hex_modal)
 
 handle_line_click({},'')
