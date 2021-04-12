0500e2ceff33a6551f415070209785f4e8c6d5a6
Joseph Scoggins <borninthecaul@gmail.com>
2021-04-12 12:46:48 -0700

    fixes archive table layout

--- profile/includes/templates/archive.php
+++ profile/includes/templates/archive.php
@@ -6,8 +6,12 @@ $archive_template = new stdClass;
 
 
 $archive_template->archive_table = function ($table) {
+  $result_table = [];
   $archive_props = [
-    'author','hex_char','body','date_time',
+    'author','hex_char','date_time',
+  ];
+  $api_props = [
+   'addressee','author','body','date_time','hex_index','mvng_lines'
   ];
   ?>
   <div id='message-archive' class='archive-list flex-col flex-start'>
@@ -20,6 +24,7 @@ $archive_template->archive_table = function ($table) {
     </div>
   <?php
   foreach($table as $row) {
+    $result_row = [];
     $row_class = (!empty($row['post_type'])) ? $row['post_type'] . '-row'  : 'no-row';
   ?>
     <div class="row-wrapper flex-row flex-center">
@@ -29,22 +34,33 @@ $archive_template->archive_table = function ($table) {
 
           $content = (!empty($row[$prop])) ? $row[$prop] : '';
           $class = $prop . ' fraggle';
+
           if ($prop==='hex_char') {
             $content = (!empty($row['hex_index'])) ?
               $hex_data->chars_arr[$row['hex_index']] : "<i class='fas fa-envelope-open-text msg-icon'></i>";
+          }
+          if ($prop==='date_time') {
+            $cron_arr = explode(' ',$row[$prop]);
+            $date_arr = explode('-',$cron_arr[0]);
+            $time_arr = explode(':',$cron_arr[1]);
+            $content = $date_arr[1] . '-' . $date_arr[2] . ' ' . $time_arr[0] . ':' . $time_arr[1];
           }
           ?>
           <div class="<?php echo $class; ?>"><?php echo $content; ?></div>
           <?php
         }
+        foreach ($api_props as $prop) {
+          $result_row[$prop] = $row[$prop] ? $row[$prop] : '';
+        }
         ?>
       </div>
     </div>
   <?php
+  $result_table[] = $result_row;
   }
   ?>
-  </div>
   <?php
+  return $result_table;
 };
 
 $archive_template->get_hex_collection = function ($index) {
