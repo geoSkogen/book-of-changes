8d94c4be95d45cc4326b3a618e3e0b4404544330
Joseph Scoggins <borninthecaul@gmail.com>
2022-03-19 21:14:58 -0700

    adds 'cushioned' effect to throw modal

--- script/throw-control.js
+++ script/throw-control.js
@@ -36,9 +36,28 @@ function reset_hexes() {
   hex_builder.moving_lines = []
 }
 
+function raise_light(n) {
+  hex_modal.style.opacity = n
+  app_shell.style.opacity = 1-n
+  n+=0.1
+  return n
+}
+
 function open_hex_modal() {
-  hex_modal.style.display = 'block'
-  app_shell.style.opacity = 0.33
+  var n = 0
+  var appear
+  setTimeout( function() { 
+      hex_modal.style.display = 'block'
+      hex_modal.style.opacity = '0'
+      appear = setInterval( function () {
+
+        n = raise_light(n)
+        if (n>=1) {
+          clearInterval(appear)
+        }
+      }, 21.32)
+      app_shell.style.opacity = 0.33
+  }, 500 )
 }
 
 function close_hex_modal() {
